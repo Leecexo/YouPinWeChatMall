36cb4f7cd18d1cd6f7b4671ddc63fb90b0670ac4
Leecexo <1217491295@qq.com>
2019-04-01 10:40:03 +0800

    serch OK

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    home----
+    <div class="search-bar">
+      <div class="serch-input">
+        <!-- <icon type="search" color="#999" /> -->
+        <input type="text" placeholder="搜索">
+      </div>
+    </div>
   </div>
 </template>
+<style>
+  .search-bar {
+    padding: 20rpx;
+    background-color: #ffd161;
+  }
+
+  .serch-input {
+    background-color: #fff;
+    font-size: 28rpx;
+    text-align: center;
+    border-radius: 10rpx;
+  }
+</style>
